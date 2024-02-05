158403bb74b77fb732cd62fa35662192066778bf
Wojtyjot <wojtek.kaczmarek@gmail.com>
2024-02-05 18:19:41 +0100

    ckpt

---
+++ src/operator-cow/run_experiment.py
@@ -0,0 +1,148 @@
+import logging
+from pathlib import Path
+
+import hydra
+import torch
+import torch.nn as nn
+import wandb
+from data_utils import COWDataset, MIODataLoader, WeightedLpRelLoss
+from models.cgpt import CGPT
+from models.mmgpt import GNOT
+from models.optimizer import AdamW
+from omegaconf import DictConfig, OmegaConf
+from torch.optim.lr_scheduler import OneCycleLR
+from train_new import train
+from utils import utils
+
+logger = logging.getLogger(__name__)
+
+
+@hydra.main(version_base=None, config_path="configs", config_name="test_exp")
+def main(config: DictConfig) -> None:
+
+    wandb.init(
+        config=OmegaConf.to_container(config, resolve=True, throw_on_missing=True),
+        project=config.wandb.project,
+        tags=config.wandb.tags,
+        anonymous=config.wandb.anonymous,
+        mode=config.wandb.mode,
+        dir=Path(config.wandb.dir).absolute(),
+    )
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    # Load the data
+    dataset = COWDataset(config.data.path)
+    train_data, test_data = torch.utils.data.random_split(dataset, [0.8, 0.2])
+
+    # save normalizer to renormalize data for plotting and evaluation
+    normalizer = dataset.y_normalizer.to(device)
+
+    train_loader = MIODataLoader(
+        train_data,
+        batch_size=config.data.batch_size,
+        shuffle=True,
+        drop_last=False,
+    )
+    test_loader = MIODataLoader(
+        test_data,
+        batch_size=config.data.batch_size,
+        shuffle=False,
+        drop_last=False,
+    )
+
+    # Load model
+    if config.model.name == "CGPT":
+
+        model = CGPT(
+            trunk_size=dataset.config["input_dim"] + dataset.config["theta_dim"],
+            branch_sizes=dataset.config["branch_sizes"],
+            output_size=dataset.config["output_dim"],
+            space_dim=2,
+            output_size=3,
+            n_layers=config.model.n_layers,
+            n_hidden=config.model.n_hidden,
+            n_head=config.model.n_head,
+            n_experts=config.model.n_experts,
+            n_inner=config.model.n_inner,
+            mlp_layers=config.model.mlp_layers,
+            attn_type=config.model.attn_type,
+            act=config.model.act,
+            ffn_dropout=config.model.ffn_dropout,
+            attn_dropout=config.model.attn_dropout,
+            horiz_fourier_dim=config.model.horiz_fourier_dim,
+        )
+
+    elif config.model.name == "GNOT":
+
+        model = GNOT(
+            trunk_size=dataset.config["input_dim"] + dataset.config["theta_dim"],
+            branch_sizes=dataset.config["branch_sizes"],
+            output_size=dataset.config["output_dim"],
+            space_dim=2,
+            output_size=3,
+            n_layers=config.model.n_layers,
+            n_hidden=config.model.n_hidden,
+            n_head=config.model.n_head,
+            n_experts=config.model.n_experts,
+            n_inner=config.model.n_inner,
+            mlp_layers=config.model.mlp_layers,
+            attn_type=config.model.attn_type,
+            act=config.model.act,
+            ffn_dropout=config.model.ffn_dropout,
+            attn_dropout=config.model.attn_dropout,
+            horiz_fourier_dim=config.model.horiz_fourier_dim,
+        )
+
+    else:
+        raise ValueError(f"Model {config.model.name} not recognized.")
+
+    # Put model on device
+    model = model.to(device)
+
+    # Load optimizer
+    optimizer = AdamW(
+        params=model.parameters(),
+        lr=config.optimizer.lr,
+        betas=(config.optimizer.beta1, config.optimizer.beta2),
+        weight_decay=config.optimizer.weight_decay,
+    )
+
+    # Load lr_scheduler
+    scheduler = OneCycleLR(
+        optimizer,
+        max_lr=config.optimizer.lr,
+        div_factor=1e4,
+        epochs=config.training.epochs,
+        steps_per_epoch=len(train_loader),
+    )
+
+    # Load loss function
+    loss_func = WeightedLpRelLoss(
+        p=2,
+        component="all",
+        normalizer=normalizer,
+    )
+
+    metric_func = WeightedLpRelLoss(
+        p=2,
+        component="all",
+        normalizer=normalizer,
+    )
+
+    result = train(
+        model=model,
+        loss_func=loss_func,
+        metric_func=metric_func,
+        train_loader=train_loader,
+        val_loader=test_loader,
+        optimizer=optimizer,
+        lr_scheduler=scheduler,
+        epochs=config.training.epochs,
+        device=device,
+        grad_clip=config.training.grad_clip,
+    )
+
+
+if __name__ == "__main__":
+    main()
--- src/operator-cow/train_new.py
+++ src/operator-cow/train_new.py
@@ -5,6 +5,7 @@ import sys
 import numpy as np
 import torch
 import torch.nn as nn
+import wandb
 from data_utils import COWDataset, MIODataLoader
 from models.optimizer import Adam
 from torch.optim.lr_scheduler import OneCycleLR
@@ -20,10 +21,129 @@ def train(
     optimizer: nn.optim.Optimizer,
     lr_scheduler: nn.optim.lr_scheduler,
     epochs: int,
-    device: torch.device,
+    device: str,
     grad_clip: float,
     start_epoch: int = 0,
     print_freq: int = 20,
-    model_save_path: str = "./data/checkpoints/",
+    model_save_path: str = "../../data/checkpoints/",
+    model_name: str = "model.pt",
+    result_name: str = "results.pt",
 ):
-    pass
+    loss_train = []
+    loss_val = []
+    loss_epoch = []
+    lr_history = []
+    it = 0
+    best_val_metric = np.inf
+
+    for epoch in range(epochs):
+        model.tarin()
+        torch.cuda.empty_cache()
+
+        for batch in train_loader:
+            loss = train_batch(
+                model=model,
+                loss_func=loss_func,
+                data=batch,
+                optimizer=optimizer,
+                lr_scheduler=lr_scheduler,
+                device=device,
+                grad_clip=grad_clip,
+            )
+            wandb.log(
+                {
+                    "L2loss": loss[0],
+                    "lr": optimizer.param_groups[0]["lr"],
+                }
+            )
+            loss = np.array(loss)
+            loss_epoch.append(loss)
+            lr_history.append(optimizer.param_groups[0]["lr"])
+            it += 1
+            log = f"epoch: [{epoch +1}/{epochs}]"
+            log += f"loss: {loss[0]:.4f}"
+
+            if it % print_freq == 0:
+                print(log)
+
+        val_result = validate_epoch(
+            model=model,
+            metric_func=metric_func,
+            valid_loader=val_loader,
+            device=device,
+        )
+        wandb.log({"val_L2_loss": val_result["metric"].mean()})
+
+        val_metric = val_result["metric"].sum()
+        loss_val.append(val_metric["metric"])
+
+        if val_metric < best_val_metric:
+            best_val_metric = val_metric
+            best_val_epoch = epoch
+            torch.save(
+                model.state_dict(),
+                os.path.join(model_save_path, model_name),
+            )
+
+        result = dict(
+            best_val_epoch=best_val_epoch,
+            best_val_metric=best_val_metric,
+            loss_train=np.asarray(loss_train),
+            loss_val=np.asarray(loss_val),
+            optimizer_state=optimizer.state_dict(),
+        )
+        pickle.dump(result, open(os.path.join(model_save_path, result_name), "wb"))
+
+    return result
+
+
+def train_batch(
+    model: nn.Module,
+    loss_func: nn.Module,
+    data: list,
+    optimizer: nn.optim.Optimizer,
+    lr_scheduler: nn.optim.lr_scheduler,
+    device: str,
+    grad_clip: float,
+):
+    optimizer.zero_grad()
+    g, u_p, g_u = data
+
+    g, u_p, g_u = g.to(device), u_p.to(device), g_u.to(device)
+
+    out = model(g, u_p, g_u)
+
+    y_pred, y = out.squeeze(), g.ndata["y"].squeeze()
+
+    loss, reg, _ = loss_func(g, y_pred, y)
+
+    loss.backward()
+    nn.utils.clip_grad_norm_(model.parameters(), grad_clip)
+    optimizer.step()
+
+    if lr_scheduler:
+        lr_scheduler.step()
+
+    return (loss.item(), reg.item())
+
+
+def validate_epoch(
+    model: nn.Module,
+    metric_func: nn.Module,
+    valid_loader: MIODataLoader,
+    device: str,
+):
+    model.eval()
+    metric_val = []
+    for _, data in enumerate(valid_loader):
+        with torch.no_grad():
+            g, u_p, g_u = data
+            g, g_u, u_p = g.to(device), g_u.to(device), u_p.to(device)
+
+            out = model(g, u_p, g_u)
+
+            y_pred, y = out.squeeze(), g.ndata["y"].squeeze()
+            _, _, metric = metric_func(g, y_pred, y)
+
+            metric_val.append(metric)
+    return dict(metric=np.mean(metric_val, axis=0))
